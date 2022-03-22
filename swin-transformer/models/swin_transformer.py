7413d28ebfd9a130d3ad48215b71a3e9ba4108ed
kyeul611 <gyul611@likelion.org>
2022-03-22 19:04:34 +0900

    2022.03.22: swin-transformer

--- swin-transformer/models/swin_transformer.py
+++ swin-transformer/models/swin_transformer.py
@@ -22,41 +22,140 @@ class Mlp(nn.Module):
 
         return x
 
-    def window_partition(x, window_size, H, W):
+def window_partition(x, window_size, H, W):
 
-        """
-        Args:
-            windows: (num_windows*B, window_size, window_size, C)
-            window_size (int): Window size
-            H (int): Height of image
-            W (int): Width of image
+    """
+    Args:
+        windows: (num_windows*B, window_size, window_size, C)
+        window_size (int): Window size
+        H (int): Height of image
+        W (int): Width of image
 
-        Returns:
-            x: (B, H, W, C)    
+    Returns:
+        x: (B, H, W, C)    
 
-        """
+    """
 
-        B, H, W, C = x.shape
-        x = x.view(B, H // window_size, window_size, W // window_size, window_size, C) # |x| = 
-        windows = x.permute(0, 1, 3, 2, 4, 5).contigous().view(-1, window_size, window_size, C) # |windows| = 
+    B, H, W, C = x.shape
+    x = x.view(B, H // window_size, window_size, W // window_size, window_size, C) # |x| = 
+    windows = x.permute(0, 1, 3, 2, 4, 5).contigous().view(-1, window_size, window_size, C) # |windows| = 
 
-        return windows
+    return windows
 
-    def window_reverse(windows, window_size, H, W):
-        """
-        Args:
-            windows: (num_windows*B, window_size, window_size, C)
-            window_size (int): Window size
-            H (int): Height of image
-            W (int): Width of image
+def window_reverse(windows, window_size, H, W):
+    """
+    Args:
+        windows: (num_windows*B, window_size, window_size, C)
+        window_size (int): Window size
+        H (int): Height of image
+        W (int): Width of image
 
-        Returns:
-            x: (B, H, W, C)
+    Returns:
+        x: (B, H, W, C)
 
-        """
+    """
 
-        B = int(windows.shape[0] / (H * W / window_size / window_size))
-        x = windows.view(B, H // window_size, W // window_size, window_size, window_size, -1)
-        x = x.permute(0, 1, 3, 2, 4, 5).contigous().view(B, H, W, -1)
+    B = int(windows.shape[0] / (H * W / window_size / window_size))
+    x = windows.view(B, H // window_size, W // window_size, window_size, window_size, -1)
+    x = x.permute(0, 1, 3, 2, 4, 5).contigous().view(B, H, W, -1)
+    
+    return x
+
+
+
+class WindowAttention(nn.Module):
+    pass
+
+class SwinTransformerBlock(nn.Module):
+    pass
+
+class PatchMerging(nn.Module):
+    pass
+
+class BasicLayer(nn.Module):
+    pass
+
+class PatchEmbed(nn.Module):    
+    r""" Image to Patch Embedding
+    
+    Args:
+        img_size (int):
+        patch_size (int):
+        in_chans (int):
+        embed_dim (int):
+        norm_layer (nn.Module, optional):
+
+    """
+
+    def __init__(self, img_size=224, patch_size=4, in_chans=3, embed_dim=96, norm_layer=None):
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        patches_resolution = [img_size[0] // patch_size[0], img_size[1] // patch_size[1]]
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.patches_resolution = patches_resolution
+        self.num_patches = patches_resolution[0] * patches_resolution[1]
+
+        self.in_chans = in_chans
+        self.embed_dim = embed_dim
+
+        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
+        if norm_layer is not None:
+            self.norm = norm_layer(embed_dim)
+        else:
+            self.norm = None
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+
+
+        assert H == self.img_size[0] and W == self.img_size[1], \
+            f"Input image size ({H}*{W}) doesn't match model({self.img_size[0]}*{self.img_size[1]})."
         
+        x = self.proj(x).flatten(2).transpose(1, 2) # B Ph*Pw C
+        if self.norm is not None:
+            x = self.norm(x)
         return x
+
+    def flops(self):
+        Ho, Wo = self.patches_resolution
+        flops = Ho * Wo * self.embed_dim * self.in_chans * (self.patch_size[0] * self.patch_size[1])
+        if self.norm is not None:
+            flops += Ho * Wo * self.embed_dim
+        return flops
+
+        
+
+class SwinTransformer(nn.Module):
+    def __init__(self, img_size=224, patch_size=4, in_chans=3, num_classes=1000, 
+                embed_dim=96, depths=[2, 2, 6, 2], num_heads=[3, 6, 12, 24],
+                window_size=7, mlp_ratio=4., qkv_bias=True, qk_scale=None,
+                drop_rate=0., attn_drop_rate=0., drop_path_rate=0.1, 
+                norm_layer=nn.LayerNorm, ape=False, path_norm=True,
+                use_checkpoint=False, **kwargs):
+
+        super().__init__()
+
+        
+
+    
+    def forward_features(self, x):
+        x = self.patch_embed(x)
+        if self.ape:
+            x = x + self.absolute_pos_embed
+        x = self.pos_drop(x)
+
+        for layer in self.layers:
+            x = layer(x)
+        
+        x = self.norm(x) # B L C
+        x = self.avgpool(x.transpose(1, 2)) # B C 1
+        x = torch.flatten(x, 1)
+        return x
+
+    def forward(self, x):
+        x = self.forward_feature(x)
+        x = self.head(x)
+        return x
+
