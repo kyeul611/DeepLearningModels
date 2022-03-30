3048a38c4f6e8e14d0f7ccf9713d24911a941f6b
kyeul611 <gyul611@likelion.org>
2022-03-30 22:50:36 +0900

    2022.03.30

---
+++ swin-transformer/models/swin_mlp.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.checkpoint as checkpoint
+from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+
+        return x
+
+def window_partition(x, window_size):
+    """
+    Args:
+        |x| = (B, H, W, C)
+        window_size = (int) window size
+
+    Return:
+        |windows| : (num_windows*B, window_size, window_size, C)
+    """
+
+    B, H, W, C = x.shape
+    x = x.view(B, H // window_size, window_size, W // window_size, window_size, C)
+    windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, window_size, window_size, C)
+    return windows
+
+def window_reverse():
+    pass
+
+class SwinMLPBlock(nn.Module):
+    pass
+
+class PatchMerging(nn.Module):
+    pass
+
+class BasicLayer(nn.Module):
+    pass
+
+class PatchEmbed(nn.Module):
+    pass
+
+class SwinMLP(nn.Module):
+    pass
