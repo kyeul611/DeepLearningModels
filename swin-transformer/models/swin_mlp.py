3e83b982e991f565afdf29fac37e8efef379d5a3
Park Kyeul <80388032+kyeul611@users.noreply.github.com>
2022-03-31 23:44:20 +0900

    2022.03.31 swin_transformer

--- swin-transformer/models/swin_mlp.py
+++ swin-transformer/models/swin_mlp.py
@@ -30,7 +30,7 @@ def window_partition(x, window_size):
         window_size = (int) window size
 
     Return:
-        |windows| : (num_windows*B, window_size, window_size, C)
+        |windows| = (num_windows*B, window_size, window_size, C)
     """
 
     B, H, W, C = x.shape
@@ -39,7 +39,22 @@ def window_partition(x, window_size):
     return windows
 
 def window_reverse():
-    pass
+    """
+    Args:
+        |windows| = (num_windows*B, window_size, window_size, C)
+        window_size: (int)Window size
+        H : (int)Height of image
+        W : (int)Width of image
+    
+    Returns:
+        |x| = (B, H, W, C)
+        
+    """
+    
+    B = int(windows.shape[0] / (H * W / window_size / window_size))
+    x = windows.view(B, H // window_size, W // window_size, window_size, window_size, -1)
+    x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, H, W, -1)
+    return x
 
 class SwinMLPBlock(nn.Module):
     pass
@@ -48,8 +63,55 @@ class PatchMerging(nn.Module):
     pass
 
 class BasicLayer(nn.Module):
-    pass
-
+    
+    def __init__(self, dim, input_resolution, depth, num_heads, window_size, mlp_ratio=4., drop=0., drop_path=0.,
+                 norm_layer=nn.LayerNorm, downsample=None, use_checkpoint=False):
+        
+        super().__init__()
+        self.dim = dim
+        self.input_resolution = input_resolution
+        self.depth = depth
+        self.use_checkpoint = use_checkpoint
+        
+        # build block
+        self.block = nn.ModuleList([
+            SwinMLPBlock(dim=dim, input_resolution=input_resolution,
+                         num_heads=num_heads, window_size=window_size,
+                         shift_size=0, if (i % 2 == 0) else window_size // 2,
+                         mlp_ratio=mlp_ratio,
+                         drop=drop,
+                         drop_path=drop_path[i] if isinstance(drop_path, list) else drop_path,
+                         norm_layer=norm_layer)
+            for i in range(depth)])
+        
+        # patch merging layer
+        if downsample is not None:
+            self.downsample = downsample(input_resolution, dim=dim, norm_layer=norm_layer)
+        else:
+            self.downsample = None
+    
+    def forward(self, x):
+        for blk in self.blocks:
+            if self.use_checkpoint:
+                x = checkpoint.checkpoint(blk, x)
+            else:
+                x = blk(x)
+        if self.downsample is not None:
+            x = self.downsample(x)
+        return x
+    
+    def extra_repr(self) -> str:
+        return f"dim={self.dim}, input_resolution={self.input_resolution}, depth={self.depth}"
+    
+    def flops(self):
+        flops = 0
+        for blk in self.blocks:
+            flops += blk.flops()
+        if self.downsample is not None:
+            flops += self.downsample.flops()
+        return flops
+        
+                             
 class PatchEmbed(nn.Module):
     pass
 
